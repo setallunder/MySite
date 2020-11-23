483a7bd657d86c0cc4f193f77db765f8d3a9d176
Victoria <vikakabishova@yandex.com>
2020-11-23 17:48:11 +0300

    first commit

---
+++ CatalogContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MySite
+{
+    public class CatalogContext : DbContext
+    {
+        public CatalogContext(DbContextOptions<CatalogContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<CatalogItem> CatalogItems { get; set; }
+
+        public DbSet<CatalogBrand> CatalogBrands { get; set; }
+
+        public DbSet<CatalogType> CatalogTypes { get; set; }
+    }
+}
